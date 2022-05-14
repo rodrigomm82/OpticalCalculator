d463f37b4c95784f3efafb7e3d088ae28af69d8c
Rodrigo M Moreira <rodrigo.mm82@hotmail.com>
2022-05-14 17:11:53 -0300

    Ajustes na tela inicial

---
+++ Optical-Calculator/src/components/LeftDrawer.vue
@@ -0,0 +1,97 @@
+<template
+  id="drawer">
+  <q-drawer
+    show-if-above
+    bordered
+  >
+    <q-list>
+      <q-item-label
+        header
+      >
+        Essential Links
+      </q-item-label>
+
+      <EssentialLink
+        id="links"
+        v-for="link in essentialLinks"
+        :key="link.title"
+        v-bind="link"
+      />
+    </q-list>
+  </q-drawer>
+
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import EssentialLink from 'components/EssentialLink'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent( {
+  name: "LeftDrawer",
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+
+    return {
+      essentialLinks: linksList
+    }
+  }
+})
+</script>
+
+<style scoped>
+#drawer {
+
+}
+#links {
+  color: #21BA45;
+}
+</style>
