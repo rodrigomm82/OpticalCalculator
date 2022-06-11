731bbdc175a89d81e8d8cf2dc244256a3b97bfb3
rodrigo.maciel <rodrigo.maciel@sparkag.com.br>
2022-06-11 11:31:49 -0300

    Inicio de configura;ao de rotas

--- Optical-Calculator/src/router/routes.js
+++ Optical-Calculator/src/router/routes.js
@@ -1,3 +1,9 @@
+import Home from "pages/Home";
+import Addition from "pages/Addition";
+import Diameter from "pages/Diameter";
+import EdgeThickness from "pages/EdgeThickness";
+import Near from "pages/Near";
+import Transposition from "pages/Transposition";
 
 const routes = [
   {
@@ -13,7 +19,42 @@ const routes = [
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/addition',
+    name: 'addition',
+    component: Addition
+  },
+  {
+    path: '/diameter',
+    name: 'diameter',
+    component: Diameter
+  },
+  {
+    path: '/edge-thickness',
+    name: 'edge-thickness',
+    component: EdgeThickness
+  },
+  {
+    path: '/near',
+    name: 'near',
+    component: Near
+  },
+  {
+    path: '/transposition',
+    name: 'transposition',
+    component: Transposition
   }
 ]
+
+/*const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})*/
 
 export default routes
