7e9b9f434c3fa9c835a9f3b23d08e37d9ad69964
rodrigo.maciel <rodrigo.maciel@sparkag.com.br>
2022-06-17 22:02:58 -0300

    Refatoração de telas e componentes

--- Optical-Calculator/src/pages/Near.vue
+++ Optical-Calculator/src/pages/Near.vue
@@ -1,10 +1,37 @@
 <template>
-  Near
+
+  <calculation-page
+    :title="title"
+    :description="description"
+    :fields="fields"
+  />
+
 </template>
 
 <script>
+import { ref } from 'vue'
+import CalculationPage from 'components/CalculationPage'
+
 export default {
-  name: "Near"
+  name: "Near",
+
+  components: {CalculationPage},
+
+  setup () {
+    const fields = ref([
+      {name: 'Spherical', ref: 0, max: 20,  min: -20, step: 0.25, color: 'green'},
+      {name: 'Cylindrical', ref: 0,  max: 0, min: -6, step: 0.25, color: 'blue'},
+      {name: 'Axis', ref: 0, max: 180, min: 0, step: 1, color: 'red'},
+      {name: 'Addition', ref: 0.25, max: 6, min: 0.25, step: 0.25, color: 'orange'}
+    ])
+
+    const title = 'Near'
+    const  description = 'Known as tired view, presbyopia means that the customer needs different degrees to see correctly'
+
+    return {
+      fields, title, description
+    }
+  }
 }
 </script>
 
